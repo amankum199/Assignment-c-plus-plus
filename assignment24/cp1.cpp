ca67378b29cf282807b360656ea533e3a8a955f7
coursera-test <amankum199@gmail.com>
2023-09-30 23:49:10 +0530

    new

--- assignment24/cp1.cpp
+++ assignment24/cp1.cpp
@@ -1,7 +1,25 @@
 /*Define a function to check whether a given number is a Prime number or not*/
-#include<iostream>
+#include <iostream>
 using namespace std;
+int prime(int n)
+{
+    int count = 0;
+    for (int i = 2; i < n / 2; i++)
+    {
+        if (n % i == 0)
+            count = 1;
+    }
+    return count;
+}
 int main()
 {
-    
+    int a;
+    cout << "Enter number" << endl;
+    cin >> a;
+    if (prime(a) == 0)
+
+        cout << "prime number";
+    else
+        cout << "Not prime number";
+    return 0;
 }
--- assignment24/cp2.cpp
+++ assignment24/cp2.cpp
@@ -1,1 +1,23 @@
 /*Define a function to find the highest value digit in a given number*/
+#include <iostream>
+using namespace std;
+int highestDigit(int x)
+{
+    int y=0,max = -1;
+    while (x)
+    {
+        y = x % 10;
+        if (max < y)
+            max = y;
+        x = x / 10;
+    }
+    return max;
+}
+int main()
+{
+    int a;
+    cout << "Enter the number" << endl;
+    cin >> a;
+    cout << "Greatest Digit is " << highestDigit(a) << endl;
+    return 0;
+}
