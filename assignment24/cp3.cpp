3cd0d5487a68f2fccf2816ac2fb9107cfb4984d1
coursera-test <amankum199@gmail.com>
2023-10-01 00:05:37 +0530

    new

--- assignment24/cp3.cpp
+++ assignment24/cp3.cpp
@@ -1,1 +1,21 @@
 /*Define a function to calculate x raised to the power y.*/
+#include<iostream>
+using namespace std;
+int pow(int x,int y)
+{
+    int m=1;
+    while(y)
+    {
+        m=m*x;
+        y--;
+    }
+    return m;
+}
+int main()
+{
+    int x,y;
+    cout<<"Enter two number"<<endl;
+    cin>>x>>y;
+    cout<<x<<" Power "<<y<<" = "<<pow(x,y);
+    return 0;
+}
