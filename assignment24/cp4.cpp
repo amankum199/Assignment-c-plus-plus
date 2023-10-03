8029bf6b3d2d2d55ec1f73a92b344cc349a21ae5
coursera-test <amankum199@gmail.com>
2023-10-03 18:34:33 +0530

    new

--- assignment24/cp4.cpp
+++ assignment24/cp4.cpp
@@ -1,1 +1,42 @@
 /*Define a function to print Pascal Triangle up to N lines*/
+#include <iostream>
+using namespace std;
+int fact(int n);
+int comb(int n, int r);
+int fact(int n)
+{
+    if (n <= 0)
+        return 1;
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int comb(int n, int r)
+{
+    int com = fact(n) / (fact(r) * fact(n - r));
+    return com;
+}
+int main()
+{
+    int n;
+    cout << "Enter the number" << endl;
+    cin >> n;
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            int k = 0;
+            if (j == 0)
+                while (k++ <= (n-i))
+                    cout << " ";
+                
+            cout << comb(i,j)<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
