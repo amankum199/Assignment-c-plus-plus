dd2f891ae8f1a510e56e464cfa9e21472a67dfce
coursera-test <amankum199@gmail.com>
2023-10-04 19:10:05 +0530

    new

--- assignment24/cp7.cpp
+++ assignment24/cp7.cpp
@@ -1,1 +1,13 @@
 /*Write a function using the default argument that is able to add 2 or 3 numbers*/
+#include <iostream>
+using namespace std;
+int add(int a, int b, int c = 0)
+{
+    return a + b + c;
+}
+int main()
+{
+    cout << "Addition of two number." << add(5, 2) << endl;
+    cout << "Addition of three number." << add(5, 2, 23);
+    return 0;
+}
