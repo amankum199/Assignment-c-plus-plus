b67b02257f4db58e0719f78129132c5b5cdc5631
coursera-test <amankum199@gmail.com>
2023-10-06 08:28:14 +0530

    new

--- assignment24/demo.cpp
+++ assignment24/demo.cpp
@@ -14,9 +14,14 @@ public:
         real = r;
         img = i;
     }
-    void print()
+    void print(Complex c)
     {
         cout << real << " + " << img << "i"<<endl;
+        cout << c.real << " + " << c.img << "i"<<endl;
+    }
+    void add(Complex c)
+    {
+        cout<<real+c.real<<" + "<<img+c.img<<"i";
     }
 };
 int main()
@@ -24,7 +29,7 @@ int main()
     Complex c1, c2;
     c1.set(3,2);
     c2.set(5,3);
-    c1.print();
-    c2.print();
+    c1.print(c2);
+    c1.add(c2);
     return 0;
 }
