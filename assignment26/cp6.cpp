25b42f4e85f6f9b9b46b5d1bbdb54f139ed52536
coursera-test <amankum199@gmail.com>
2024-01-19 23:15:53 +0530

    new

--- assignment26/cp6.cpp
+++ assignment26/cp6.cpp
@@ -1,2 +1,31 @@
 /* Define a class student and write a program to enter student details using constructor
  and define member function to display all the details.*/
+ #include<iostream>
+ using namespace std;
+ class Student
+ {
+ private:
+    int marks;
+    char grade;
+ public:
+    Student(int m,char g)
+    {
+        marks=m;
+        grade=g;
+    }
+    void show()
+    {
+        cout<<"The Student obtainesd "<<marks<<" and his grade is "<<grade<<endl;
+    }
+    ~Student()
+    {
+
+    }
+ };
+ int main()
+ {
+    Student s1(84,'B'), s2(87, 'A');
+    s1.show();
+    s2.show();
+    return 0;
+ }
--- assignment26/cp7.cpp
+++ assignment26/cp7.cpp
@@ -1,4 +1,36 @@
 /* Define a class Box and write a program to enter length, breadth and height and
  initialise objects using constructor also define member functions to calculate volume
  of the box*/
- 
+#include <iostream>
+using namespace std;
+class Box
+{
+private:
+    int length, breadth, height;
+    int vol;
+
+public:
+    Box(int l, int b, int h)
+    {
+        length = l;
+        breadth = b;
+        height = h;
+    }
+    int volume()
+    {
+        vol = length * breadth * height;
+        return vol;
+    }
+    ~Box()
+    {
+    }
+};
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the length breadth and height of the box."<<endl;
+    cin>>a>>b>>c;
+    Box b1(a,b,c);
+    cout<<"The volume of the box is "<<b1.volume()<<endl;
+    return 0;
+}
