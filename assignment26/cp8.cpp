3b370678a8df54a85ed1a587d4e632ea5ba69316
coursera-test <amankum199@gmail.com>
2024-01-20 14:57:15 +0530

    new

--- assignment26/cp8.cpp
+++ assignment26/cp8.cpp
@@ -1,3 +1,33 @@
 /*Define a class Bank and define member functions to read principal , rate of interest
  and year. Another member functions to calculate simple interest and display it.
  Initialise all details using constructor*/
+#include <iostream>
+using namespace std;
+class Bank
+{
+private:
+    int principal, rateOfInterest, year, si;
+
+public:
+    Bank(int p, int r, int y)
+    {
+        principal = p;
+        rateOfInterest = r;
+        year = y;
+    }
+    int calculate()
+    {
+        si = (principal * rateOfInterest * year) / 100;
+        return si;
+    }
+    ~Bank()
+    {
+        
+    }
+};
+int main()
+{
+    Bank b1(10000,3,2);
+    cout<<"Simple Interest is "<<b1.calculate()<<endl;
+    return 0;
+}
