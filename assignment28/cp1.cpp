483405dba0ec62beec7a1a5899f8a1f2d96f66c1
coursera-test <amankum199@gmail.com>
2024-04-11 02:52:49 +0530

    n

--- assignment28/cp1.cpp
+++ assignment28/cp1.cpp
@@ -2,3 +2,41 @@
 Overload following operators
 a. << insertion operator
 b. >> extraction operator*/
+#include <iostream>
+using namespace std;
+class complex
+{
+private:
+    int real;
+    int img;
+
+public:
+    complex(int r,int i)
+    {
+        real=r;
+        img=i;
+    }
+    complex()
+    {}
+    ~complex()
+    {}
+    friend  istream& operator>>(istream &in, complex &c2)
+    {
+        cout<<"Enter real and Imaginary value:"<<endl;
+        in>>c2.real>>c2.img;
+    }
+    friend ostream& operator<<(ostream &os, complex &c1)
+    {
+        os << "Real = " << c1.real << " Img = " << c1.img << endl;
+    }
+};
+int main()
+{
+    complex c3(3,5);
+    complex c2(2,7);
+    complex c1;
+    cin>>c1;
+    cout<<c1;
+    cout<<c2<<c3<<endl;
+
+}
--- assignment28/cp2.cpp
+++ assignment28/cp2.cpp
@@ -1,3 +1,31 @@
 /*Define a class Complex with appropriate instance variables and member functions.
 One of the functions should be setData() to set the properties of the object. Make
 sure the names of formal arguments are the same as names of instance variables*/
+#include <iostream>
+using namespace std;
+class complex
+{
+private:
+    int real;
+    int img;
+
+public:
+    void setdata(int real, int img)
+    {
+        this->real=real;
+        this->img=img;
+    }
+    friend ostream& operator<<(ostream &os, complex &c1)
+    {
+        os << "Real = " << c1.real << " Img = " << c1.img << endl;
+    }
+};
+int main()
+{
+    complex c2;
+    complex c1;
+    c1.setdata(3,5);
+    c2.setdata(5,9);
+    cout<<c1<<c2<<endl;
+
+}
