f9348a8cea0898a380b7c142a387a9a1b6f07d95
coursera-test <amankum199@gmail.com>
2024-04-13 12:42:23 +0530

    new

--- assignment29/cp5.cpp
+++ assignment29/cp5.cpp
@@ -1,4 +1,4 @@
-/*Create two classes Invent1 and Invent2 and also add necessary constructors in it. Now add 
+/*Create two classes Invent1 and Invent2 and also add necessary constructors in it. Now add
 functions to support Invent1 to float and Invent1 to Invent2 type.
 Example -
 int main()
@@ -10,3 +10,60 @@ int main()
  y=x;  // Invent1 to Invent2
  return 0;
 }*/
+#include <iostream>
+using namespace std;
+class invent1
+{
+private:
+    int a, b;
+
+public:
+    invent1() {}
+    invent1(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+    operator float()
+    {
+        return (a + b);
+    }
+    int geta()
+    {
+        return a;
+    }
+    int getb()
+    {
+        return b;
+    }
+};
+class invent2
+{
+    private:
+    int i,j;
+    public:
+    invent2(){}
+    invent2(invent1 a1)
+    {
+        i=a1.geta();
+        j=a1.getb();
+
+    }
+    void display()
+    {
+        cout<<i<<" "<<j<<endl;
+    }
+
+};
+
+int main()
+{
+    invent1 x(3, 4);
+    invent2 y;
+    float z;
+    z = x; // Invent1 to float
+    cout<<z<<endl;
+    y = x; // Invent1 to Invent2
+    y.display();
+    return 0;
+}
