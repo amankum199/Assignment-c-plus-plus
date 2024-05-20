2fa89a99be22d81e1bcea476ca3b36c1c63224bc
coursera-test <amankum199@gmail.com>
2024-05-21 01:03:42 +0530

    new

--- assignment34/cp6.cpp
+++ assignment34/cp6.cpp
@@ -1,2 +1,31 @@
 /* Write a C++ program that counts the total number of characters, words and lines in
 the file.*/
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    ofstream fout;
+    fout.open("D:/file/count.txt");
+    fout<<"my name is aman"<<endl;
+    fout.close();
+    ifstream fin;
+    fin.open("D:/file/count.txt");
+    fin.seekg(0,ios_base::beg);
+    char ch;
+    int i,c=0,sp=0;
+    while(!fin.eof())
+    {
+        fin.get(ch);
+        if((ch>63&&ch<91)||(ch>96&& ch<123))
+        c++;
+        if(ch==' ')
+        sp++;
+    }
+    cout<<"No. of character in a file: "<<c<<endl;
+    cout<<"No. of words  in a file: "<<(sp+1)<<endl;
+    fin.close();
+
+    return 0;
+}
--- assignment34/cp7.cpp
+++ assignment34/cp7.cpp
@@ -2,3 +2,28 @@
 characters for item-name and an integer price. Write a program to read these
 records, arrange them in the descending order of price and write them in the same
 file, overwriting the earlier records.*/
+#include<iostream>
+#include<fstream>
+#include<string.h>
+using namespace std;
+class record
+{
+    public:
+    int item_code;
+    string item_name;
+    int price;
+
+
+
+};
+int main()
+{
+    ifstream fin;
+    fin.open("D:/file/abc.txt");
+    for(int i=1;i<=50;i++)
+    {   
+        cout<<"Enter record of student: "<<i<<endl;
+        
+    }
+    return 0;
+}
